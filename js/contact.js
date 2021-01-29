594325cf6232172d41a66ade4b7f8ac9e46ed3c5
Harya J <harya.pratama22@gmail.com>
2021-01-29 19:33:24 +0700

    Update contact form

--- js/contact.js
+++ js/contact.js
@@ -1,47 +1,46 @@
-$(function () {
+ window.addEventListener("DOMContentLoaded", function () {
 
-    "use strict";
+     // get the form elements defined in your form HTML above
 
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
+     var form = document.getElementById("my-form");
+     //  var button = document.getElementById("submit");
+     var status = document.getElementById("status");
 
-    $('#contact-form').validator();
+     // Success and Error functions for after the form is submitted
 
+     function success() {
+         form.reset();
+         //  status.classList.add("alert alert-succes alert-dismissible fade show");
+         status.innerHTML = "Your message has been sent! Thanks for contacting me.";
+     }
 
-    // when the form is submitted
-    $('#contact-form').on('submit', function (e) {
+     function error() {
+         //  status.classList.add("alert alert-warning alert-dismissible fade show");
+         status.innerHTML = "Oops! There was a problem.";
+     }
 
-        // if the validator does not prevent form submit
-        if (!e.isDefaultPrevented()) {
-            var url = "form/contact.php";
+     // handle the form submission event
 
-            // POST values in the background the the script URL
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    // data = JSON object that contact.php returns
+     form.addEventListener("submit", function (ev) {
+         ev.preventDefault();
+         var data = new FormData(form);
+         ajax(form.method, form.action, data, success, error);
+     });
+ });
 
-                    // we recieve the type of the message: success x danger and apply it to the 
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
+ // helper function for sending an AJAX request
 
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            return false;
-        }
-    })
-});
+ function ajax(method, url, data, success, error) {
+     var xhr = new XMLHttpRequest();
+     xhr.open(method, url);
+     xhr.setRequestHeader("Accept", "application/json");
+     xhr.onreadystatechange = function () {
+         if (xhr.readyState !== XMLHttpRequest.DONE) return;
+         if (xhr.status === 200) {
+             success(xhr.response, xhr.responseType);
+         } else {
+             error(xhr.status, xhr.response, xhr.responseType);
+         }
+     };
+     xhr.send(data);
+ }
